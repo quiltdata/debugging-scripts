a763540a124d21d626b4c84828f5a30ae11dba2a
armandmcqueen <armandmcqueen@gmail.com>
2019-11-19 21:41:49 -0800

    Quick update

--- debug.sh
+++ debug.sh
@@ -11,6 +11,5 @@ python quilt-tmp/debugging-scripts/package_deletion_problem.py > quilt-tmp/quilt
 #python package_deletion_problem.py > quilt-tmp/quilt-pkg-delete-debug.log 2> /dev/null
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 AWS_ACCESS_KEY_ID=${QUILT_DEBUG_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${QUILT_DEBUG_SECRET_ACCESS_KEY} AWS_REGION=us-east-1 aws s3 cp quilt-tmp/quilt-pkg-delete-debug.log s3://quilt-support/hudl/${TIMESTAMP}/quilt-pkg-delete-debug.log
-cd ../..
 rm -rf ./quilt-tmp/
 echo "Done!"
