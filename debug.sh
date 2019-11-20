fe46adccc2dad4f2e1de9e2b26d9b84ca863d39c
armandmcqueen <armandmcqueen@gmail.com>
2019-11-19 16:18:23 -0800

    Quick update

--- debug.sh
+++ debug.sh
@@ -5,7 +5,7 @@
 quilt3 logout
 mkdir -p quilt-tmp
 echo "QUILT-DEBUG: Cloning debugger script repo"
-git clone https://github.com/quiltdata/debugging-scripts quilt-tmp/debugging-scripts
+git clone https://github.com/quiltdata/debugging-scripts -b hudl quilt-tmp/debugging-scripts
 echo "QUILT-DEBUG: Running python code that debugs"
 python quilt-tmp/debugging-scripts/package_deletion_problem.py > quilt-tmp/quilt-pkg-delete-debug.log
 #python package_deletion_problem.py > quilt-tmp/quilt-pkg-delete-debug.log 2> /dev/null
