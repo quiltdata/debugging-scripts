02efa3a661cbe56feb48963e1eebcc28613c96a9
armandmcqueen <armandmcqueen@gmail.com>
2019-11-19 16:02:34 -0800

    Added notes

--- debug.sh
+++ debug.sh
@@ -2,11 +2,13 @@
 
 quilt3 logout
 mkdir -p
+echo "QUILT-DEBUG: Cloning debugger script repo"
 git clone https://github.com/quiltdata/debugging-scripts quilt-tmp/debugging-scripts
 cd quilt-tmp
+echo "QUILT-DEBUG: Running python code that debugs"
 python package_deletion_problem.py > quilt-pkg-delete-debug.log 2> /dev/null
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo ${TIMESTAMP}
 AWS_ACCESS_KEY_ID=${QUILT_DEBUG_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${QUILT_DEBUG_SECRET_ACCESS_KEY} AWS_REGION=us-east-1 aws s3 cp quilt-pkg-delete-debug.log s3://quilt-support/hudl/${TIMESTAMP}/quilt-pkg-delete-debug.log
 cd ..
-rm -rf ./quilt-tmp/
+# rm -rf ./quilt-tmp/
